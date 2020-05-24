e424c6ee961f03f740bd5804c745ae505c0a9ce3
Philipp Nueesch <philipp.nueesch@gmail.com>
2020-05-24 14:05:30 +0800

    Fix form row styling issue

--- src/app/user/account/account.component.ts
+++ src/app/user/account/account.component.ts
@@ -1,31 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UserService } from '../user.service';
-import { User } from '../user.model';
-import { takeUntil, tap } from 'rxjs/operators';
-import { BaseComponent } from 'src/app/shared/components/base/base.component';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss']
 })
-export class AccountComponent extends BaseComponent implements OnInit {
-  currentUser: User;
+export class AccountComponent {
 
-  constructor(private user: UserService) {
-    super();
-  }
+  constructor(public user: UserService) { }
 
-  ngOnInit(): void {
-    this.user.currentUser$
-      .pipe(
-        takeUntil(this.destroyed$),
-        tap(user => {
-          if (user) {
-            this.currentUser = user;
-          }
-        })
-      )
-      .subscribe();
-  }
 }
